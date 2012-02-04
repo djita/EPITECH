8ef92d5f79e76269419ad4181e276b24ef65a5ba
guillo_e <lyoma.guillou@epitech.eu>
2012-02-04 16:01:35 +0100

    Script re-edited

--- GitHubPush.sh
+++ GitHubPush.sh
@@ -2,7 +2,7 @@ cp -r ~/Documents/* ~/GitTimeMachine/EPITECH/
 date >> date.log
 echo ' Commited by '>> date.log
 echo $USER >> date.log
-echo '\n' >> date.log
+echo "\n" >> date.log
 git add *
 git commit
 git push
